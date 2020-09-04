ece5574373757334eae80ac5460f4d8c8901b69b
jake0988 <jake098877@gmail.com>
2020-09-03 20:17:12 -0400

    added house_cup_winner migration

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,24 +16,26 @@ class StudentsController < ApplicationController
     end
 
     post '/students' do
+        if Student.find_by(:username => params[:student][:username])
+            flash[:message] = "Username already taken"
+            flash[:message] 
+            redirect :'/signup'
+        end
         @student = Student.create(:username => params[:student][:username])
         @student.first_name = params[:student][:first]
         @student.last_name = params[:student][:last]
-        @student.password = params[:student][:password]
+        @student.password = params[:password]
         if params[:student][:house]
-        @student.house_id = params[:student][:house].first.to_i
+        @student.house_id = params[:student][:house].to_i
         end
+binding.pry
         @student.save
+        session[:student_id] = @student.id
+
         if params[:student][:username] == "Dumbledore" && @student_id
-            session[:admin_id] == @student.id
-        end
-        if @student.errors.messages
-            # session[:error] = @student.errors.messages
-            flash[:message] = @student.errors.messages
-            redirect '/signup'
-      
-        elsif @student.id
-            redirect "/students/#{@student.id}"
+            session[:admin_id] = @student.id.to_s
+        elsif session[:student_id]
+            redirect "/students/#{session[:student_id]}"
         else
             redirect "/"
         end
@@ -52,9 +54,8 @@ class StudentsController < ApplicationController
     end
 
     post '/login' do
-        
-        student = Student.find_by(username: params[:username])
-        if params[:username] = "Dumbledore"
+        student = Student.find_by(username: params[:student][:username])
+        if student && params[:student][:username] = "Dumbledore"
             session[:admin_id] = student.id
         end
         if student && student.authenticate(params[:password])
@@ -68,7 +69,29 @@ class StudentsController < ApplicationController
     get '/students/:id' do
         if logged_in?
             @student = Student.find(session[:student_id])  
+            binding.pry
+            if @student.house_id
+                @house = @student.house_id
+            else
+                @house = []
+            end
+            if @student.last_name
+                @last = @student.last_name
+            else
+                @last = []
+            end
+            
+            if @student.first_name
+                @first = @student.first_name
+            else
+                @first = []
+            end
     
+            if @student.cup_winners.last != nil
+                @cup = @student.cup_winners.last.year
+            else
+                @cup = []
+            end
             erb :"/students/show"
         else
             redirect '/login'
@@ -78,6 +101,7 @@ class StudentsController < ApplicationController
     post '/students/:id' do
         if logged_in?
         @student = Student.find(session[:id])
+        
         redirect "/students/show"
         else
             redirect '/login'
