7e0ea7b3cc93141e4725111f012fe6cc48537f6f
jake0988 <jake098877@gmail.com>
2020-09-07 14:55:38 -0400

    Some extra code removed

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,12 +5,12 @@ class Student < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true, uniqueness: true 
     validates :house, presence: true
-    def slug
-        username.downcase.gsub(" ","-")
-      end
+    # def slug
+    #     username.downcase.gsub(" ","-")
+    #   end
     
-      def self.find_by_slug(slug)
-        Student.all.find{|student| student.slug == slug}
-      end
+    #   def self.find_by_slug(slug)
+    #     Student.all.find{|student| student.slug == slug}
+    #   end
     
 end
